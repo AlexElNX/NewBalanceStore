fc147468eb27983f991eee4e3361a7ecde5885e7
Alex <stdcpp@gmail.com>
2026-06-02 11:48:35 -0500

    finish home page

--- js/app.js
+++ js/app.js
@@ -1,91 +1,40 @@
-import { Product } from './entities/Product.js';
-import { Cart } from './entities/Cart.js';
-import { Category } from "./entities/Category.js";
-import { Customer } from "./entities/Customer.js";
-import { Manufacturer } from "./entities/Manufacturer.js";
-import { Order } from "./entities/Order.js";
+// import { Product } from './entities/Product.js';
+// import { Cart } from './entities/Cart.js';
+// import { Category } from "./entities/Category.js";
+// import { Customer } from "./entities/Customer.js";
+// import { Manufacturer } from "./entities/Manufacturer.js";
+// import { Order } from "./entities/Order.js";
 
-const manufacturer = new Manufacturer(
-  "New Balance",
-  "USA",
-  1906,
-  "https://www.newbalance.com"
-);
+const header = document.querySelector('.main-header');
+const hero = document.querySelector('.hero');
 
-const category = new Category(
-  "Running Shoes",
-  "Shoes for running and everyday wear",
-  [],
-  "Unisex",
-  "All season"
-);
+let lastScroll = 0;
 
-const product1 = new Product(
-  "New Balance 9060",
-  220,
-  180,
-  44,
-  "Gray",
-  category.name,
-  "Comfortable lifestyle sneakers",
-  manufacturer.name,
-  true
-);
+window.addEventListener('scroll', () => {
+  const currentScroll = window.scrollY;
 
-const product2 = new Product(
-  "New Balance 530",
-  140,
-  110,
-  43,
-  "White",
-  category.name,
-  "Classic running sneakers",
-  manufacturer.name,
-  true
-);
+  if (currentScroll <= 0) {
+    header.style.position = 'absolute';
+    header.style.width = '100%';
+    header.style.left = '0';
 
-manufacturer.addProduct(product1);
-manufacturer.addProduct(product2);
+    header.classList.remove('fixed', 'hidden', 'scrolled');
+    lastScroll = 0;
+    return;
+  }
 
-category.addProduct(product1);
-category.addProduct(product2);
+  header.style.position = 'fixed';
+  header.style.width = `${hero.offsetWidth}px`;
+  header.style.left = `${hero.getBoundingClientRect().left}px`;
 
-const cart = new Cart(
-  [],
-  0,
-  15,
-  10
-);
+  header.classList.add('scrolled');
+  header.classList.add('fixed');
 
-cart.addProduct(product1);
-cart.addProduct(product2);
+  if (currentScroll > lastScroll) {
+    header.classList.add('hidden');
+  } else {
+    header.classList.remove('hidden');
+  }
 
-const customer = new Customer(
-  "Jon",
-  "jon@gmail.com",
-  "New York, USA",
-  []
-);
-
-const order = new Order(
-  "ORD-001",
-  cart.products,
-  customer.name,
-  "Processing",
-  "05.28.2026"
-);
-
-customer.orders.push(order);
-
-product1.print();
-product2.print();
-
-manufacturer.printInfo();
-
-category.showProducts();
-
-customer.showOrders();
-
-order.printOrder();
-
-console.log(cart);
+  lastScroll = currentScroll;
+});
