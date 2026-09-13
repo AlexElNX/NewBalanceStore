58be51315fb71986aaf0bef8bf420dc33fbf3fbd
kasatjk <kstk.collar319@slmail.me>
2026-09-13 16:25:05 +0300

    Tests

---
+++ backend/NewBalanceStore.UnitTests/Helpers/TestDataBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBalanceStore.UnitTests.Helpers
+{
+    internal class TestDataBuilder
+    {
+    }
+}
---
+++ backend/NewBalanceStore.UnitTests/Mappings/DtoMappingTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBalanceStore.UnitTests.Mappings
+{
+    internal class DtoMappingTests
+    {
+    }
+}
