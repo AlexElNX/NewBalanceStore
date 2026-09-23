3ff148f7521e48baf32cb7595d7af813d8173da3
ZemNyux <nikita.gaber2703@gmail.com>
2026-09-23 18:34:54 +0300

    feat: add Cloudinary image upload endpoint
    - Added PhotoService for uploading product images to Cloudinary
    - Created POST upload-image endpoint in ProductsController

---
+++ NewBalanceStore.Application/Interfaces/IPhotoService.cs
@@ -0,0 +1,9 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace NewBalanceStore.Application.Interfaces;
+
+public interface IPhotoService
+{
+    Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+}
